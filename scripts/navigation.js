4dc399f8fef353c2bcb60c34608d79b3d8e0b56f
Pierre Marquet <marquet_pierre@yahoo.fr>
2021-09-15 12:44:57 +0200

    update

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,26 +1,97 @@
 $(document).ready(function () {
+
     let currentViewIndex = 0;
-    let lastScrollTop = 0;
 
+    toggleViews(currentViewIndex);
 
-    $(window).scroll(function () {
-        let curentScrollTop = $(this).scrollTop();
-        if (curentScrollTop > lastScrollTop) {
-            currentViewIndex++
-        } else {
-            currentViewIndex--
-        }
+    function toggleViews(currentViewIndex) {
         console.log(currentViewIndex);
-        lastScrollTop = curentScrollTop;
-    });
-
-   function toggleViewsUp(currentViewIndex) {
-
+        $('.mainDiv').addClass("none");
+        $('#previousPageButton').removeClass("none")
+        $('#nextPageButton').removeClass("none")
+        switch (currentViewIndex) {
+            case 0:
+                $('#home').removeClass("none");
+                $('#previousPageButton').addClass("none")
+                $('#nextPageButton').addClass("none")
+                break;
+            case 1:
+                $('#weSplitch').removeClass("none");
+                break;
+            case 2:
+                $('#newSocialNetwork').removeClass("none");
+                break;
+            case 3:
+                $('#passion').removeClass("none");
+                break;
+            case 4:
+                $('#emotion').removeClass("none");
+                break;
+            case 5:
+                $('#connect').removeClass("none");
+                break;
+            case 6:
+                $('#exchange').removeClass("none");
+                break;
+            case 7:
+                $('#beFirst').removeClass("none");
+                break;
+            case 8:
+                $('#enterInGame').removeClass("none");
+                $('#nextPageButton').addClass("none")
+                break;
+            case 9:
+                $('#reserve').removeClass("none");
+                // $('#nextPageButton').addClass("none")
+                break;
+            case 10:
+                $('#putAside').removeClass("none");
+                break;
+            case 11:
+                $('#whatYouThrill').removeClass("none");
+                break;
+            case 12:
+                $('#tags').removeClass("none");
+                // $('#nextPageButton').addClass("none")
+                break;
+            case 13:
+                $('#cantWaitToThrill').removeClass("none");
+                break;
+            case 14:
+                $('#whatBadge').removeClass("none");
+                // $('#nextPageButton').addClass("none")
+                break;
+            case 15:
+                $('#goodChoice').removeClass("none");
+                break;
+            case 16:
+                $('#whatMail').removeClass("none");
+                // $('#nextPageButton').addClass("none")
+                break;
+            case 17:
+                $('#prepareProfile').removeClass("none");
+                break;
+            case 18:
+                $('#followUs').removeClass("none");
+                $('#nextPageButton').addClass("none")
+                break;
+            default:
+                break;
+        }
     }
 
-    $('.bordBtn').click(function () {
-        console.log("click")
+    $('#nextPageButton').click(function () {
         currentViewIndex++
-        // toggleViews(currentViewIndex);
-    })
+        toggleViews(currentViewIndex);
+    });
+    
+    $('#previousPageButton').click(function () {
+        currentViewIndex--
+        toggleViews(currentViewIndex);
+    });
+
+    $('.bordBtn').click(function () {
+        currentViewIndex++
+        toggleViews(currentViewIndex);
+    });
 });
