42c4646289d4a353840c7663ebbc22a7272c7ef6
Pierre Marquet <marquet_pierre@yahoo.fr>
2021-09-15 15:57:29 +0200

    update

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -4,87 +4,11 @@ $(document).ready(function () {
 
     toggleViews(currentViewIndex);
 
-    function toggleViews(currentViewIndex) {
-        console.log(currentViewIndex);
-        $('.mainDiv').addClass("none");
-        $('#previousPageButton').removeClass("none")
-        $('#nextPageButton').removeClass("none")
-        switch (currentViewIndex) {
-            case 0:
-                $('#home').removeClass("none");
-                $('#previousPageButton').addClass("none")
-                $('#nextPageButton').addClass("none")
-                break;
-            case 1:
-                $('#weSplitch').removeClass("none");
-                break;
-            case 2:
-                $('#newSocialNetwork').removeClass("none");
-                break;
-            case 3:
-                $('#passion').removeClass("none");
-                break;
-            case 4:
-                $('#emotion').removeClass("none");
-                break;
-            case 5:
-                $('#connect').removeClass("none");
-                break;
-            case 6:
-                $('#exchange').removeClass("none");
-                break;
-            case 7:
-                $('#beFirst').removeClass("none");
-                break;
-            case 8:
-                $('#enterInGame').removeClass("none");
-                $('#nextPageButton').addClass("none")
-                break;
-            case 9:
-                $('#reserve').removeClass("none");
-                // $('#nextPageButton').addClass("none")
-                break;
-            case 10:
-                $('#putAside').removeClass("none");
-                break;
-            case 11:
-                $('#whatYouThrill').removeClass("none");
-                break;
-            case 12:
-                $('#tags').removeClass("none");
-                // $('#nextPageButton').addClass("none")
-                break;
-            case 13:
-                $('#cantWaitToThrill').removeClass("none");
-                break;
-            case 14:
-                $('#whatBadge').removeClass("none");
-                // $('#nextPageButton').addClass("none")
-                break;
-            case 15:
-                $('#goodChoice').removeClass("none");
-                break;
-            case 16:
-                $('#whatMail').removeClass("none");
-                // $('#nextPageButton').addClass("none")
-                break;
-            case 17:
-                $('#prepareProfile').removeClass("none");
-                break;
-            case 18:
-                $('#followUs').removeClass("none");
-                $('#nextPageButton').addClass("none")
-                break;
-            default:
-                break;
-        }
-    }
-
     $('#nextPageButton').click(function () {
         currentViewIndex++
         toggleViews(currentViewIndex);
     });
-    
+
     $('#previousPageButton').click(function () {
         currentViewIndex--
         toggleViews(currentViewIndex);
@@ -94,4 +18,99 @@ $(document).ready(function () {
         currentViewIndex++
         toggleViews(currentViewIndex);
     });
+
 });
+
+//////////////////////////////// Function ////////////////////////////////
+
+function toggleViews(currentViewIndex) {
+    console.log(currentViewIndex);
+    $('.mainDiv').addClass("none");
+    $('#previousPageButton').removeClass("none")
+    $('#nextPageButton').removeClass("none")
+    switch (currentViewIndex) {
+        case 0:
+            $('#home').removeClass("none");
+            $('#previousPageButton').addClass("none")
+            $('#nextPageButton').addClass("none")
+            break;
+        case 1:
+            $('#weSplitch').removeClass("none");
+            break;
+        case 2:
+            $('#newSocialNetwork').removeClass("none");
+            break;
+        case 3:
+            $('#passion').removeClass("none");
+            break;
+        case 4:
+            $('#emotion').removeClass("none");
+            break;
+        case 5:
+            $('#connect').removeClass("none");
+            break;
+        case 6:
+            $('#exchange').removeClass("none");
+            break;
+        case 7:
+            $('#beFirst').removeClass("none");
+            break;
+        case 8:
+            $('#enterInGame').removeClass("none");
+            $('#nextPageButton').addClass("none")
+            break;
+        case 9:
+            $('#reserve').removeClass("none");
+            // $('#nextPageButton').addClass("none")
+            break;
+        case 10:
+            $('#putAside').removeClass("none");
+            break;
+        case 11:
+            $('#whatYouThrill').removeClass("none");
+            break;
+        case 12:
+            $('#tags').removeClass("none");
+            // $('#nextPageButton').addClass("none")
+            break;
+        case 13:
+            $('#cantWaitToThrill').removeClass("none");
+            break;
+        case 14:
+            $('#whatBadge').removeClass("none");
+            // $('#nextPageButton').addClass("none")
+            break;
+        case 15:
+            $('#goodChoice').removeClass("none");
+            break;
+        case 16:
+            $('#whatMail').removeClass("none");
+            // $('#nextPageButton').addClass("none")
+            break;
+        case 17:
+            $('#prepareProfile').removeClass("none");
+            break;
+        case 18:
+            $('#followUs').removeClass("none");
+            $('#nextPageButton').addClass("none")
+            break;
+        default:
+            break;
+    }
+}
+
+// function typeEffect(element, speed) {
+//     var text = $(element).html();
+//     console.log(text)
+//     // $(element).html("");
+
+//     var i = 0;
+//     var timer = setInterval(function () {
+//         if (i < text.length) {
+//             element.append(text.charAt(i));
+//             i++;
+//         } else {
+//             clearInterval(timer);
+//         }
+//     }, speed);
+// }
